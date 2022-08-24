3bf890f0f6b46400197ff105b0a45d11abc48bba
SantiagoVieira <111204758+SantiagoVieira@users.noreply.github.com>
2022-08-23 21:33:34 -0500

    Update Reto 4.py

--- Reto 4.py
+++ Reto 4.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from datetime import date
 
-
 class User():
     def __init__(self,id,user_name,balance):
         self.id=int(id)
@@ -13,28 +12,25 @@ class User():
     
     def add_product_to_car(self,product):
         self.car.append(product)
-
     
     def consolidate_order(self,order_id):
 
         for i in self.car:
             order_id.total+=i.price
         if order_id.total>self.balance:
-            print(f" NO TIENE SALDO SUFICIENTE \n El valor total de su compra es de {order_id.total} y su saldo es de {self.balance}$ ")
+            print(f" Actualmente no tienes suficiente saldo y El valor  de su compra es de {order_id.total} y su saldo actual es de {self.balance}$ ")
         else:
-            print(f" La orden se ha realizado con exito.\nEl valor a pagar es de {order_id.total}")
+            print(f" Su orden a sido exitosa.\n Valor a pagar es  {order_id.total}")
             order_id.product_list.append(self.car)
             self.order_list.append(order_id)
             order_id.status=True
     
 
-    
     def add_to_balance(self,amount):
         if type(amount)==int:
             self.balance+=amount
         else:
-            print("digite un número valido")
-
+            print("Nuevamente ingrese un numero valido")
     
     def plot_order_history(self,product):
         x=[]
@@ -51,13 +47,10 @@ class User():
             plt.bar(x,y,linestyle="--",color="g",label=product.name)
 
         plt.legend()
-        plt.xlabel("Tiempo")
-        plt.ylabel("Precio")
-        plt.title(f"Cambio del precio de la {product.name}")
+        plt.xlabel("Time")
+        plt.ylabel("Price")
+        plt.title(f"Cambio del {product.name}")
         plt.show()
-
-
-        
 
 class Order():
     def __init__(self,id,date,status=False,total=0):
@@ -78,24 +71,24 @@ class Product():
         self.price=float(price)
         self.price_history[date]=price
 
-Arroz=Product(1,"Arroz",123,{date(2022,1,12):123.7})
-Frijoles=Product(2,"Frijoles",12,{date(2022,1,19):12})
-Lentejas=Product(3,"Lentejas",34,{date(2022,1,2):34})
-Chocolate=Product(4,"Chocolate",42,{date(2022,1,5):42})
-Galletas=Product(5,"Galletas",54,{date(2022,1,4):54})
+Arroz=Product(1,"Arroz",123,{date(2022,1,11):156.7})
+Frijoles=Product(2,"Frijoles",12,{date(2022,1,18):18})
+Lentejas=Product(3,"Lentejas",34,{date(2022,1,33):37})
+Chocolate=Product(4,"Chocolate",42,{date(2022,1,55):49})
+Galletas=Product(5,"Galletas",54,{date(2022,1,44):52})
 
-papa.update_price(date(2022,3,12),2.2)
+Arroz.update_price(date(2022,3,12),2.5)
 
 Santiago=User(1,"Santiago VIeira",13.12)
 Santiago.add_product_to_car(Frijoles)
 Santiago.add_product_to_car(Lentejas)
 Santiago.add_product_to_car(Chocolate)
 
-mercado=Order(1,date(2022,1,2))
+mercado=Order(1,date(2022,2,2))
 Santiago.consolidate_order(mercado)
 
-Santiago.add_to_balance(3123123)
-mercado=Order(1,date(2022,1,2))
+Santiago.add_to_balance(3214569)
+mercado=Order(1,date(2022,2,2))
 Santiago.consolidate_order(mercado)
 
 Santiago.plot_order_history(Arroz)
